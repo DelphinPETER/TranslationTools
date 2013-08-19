ef37704f6a5128e61898ff022f7df3165903011b
Del <del@PC-Del.(none)>
2013-08-19 22:54:46 +0200

    Add slovak translation

---
+++ source/language_sk.ts
@@ -0,0 +1,140 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="sk">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="MainWindow.ui" line="14"/>
+        <source>CyanogenMod Translation Tool - Debug</source>
+        <translation>Nástroj CyanogenMod Translation Tool - Ladenie</translation>
+    </message>
+    <message>
+        <location filename="MainWindow.ui" line="27"/>
+        <source>Language :</source>
+        <translation>Jazyk:</translation>
+    </message>
+    <message>
+        <location filename="MainWindow.ui" line="57"/>
+        <source>Scan</source>
+        <translation>Preskúmať</translation>
+    </message>
+    <message>
+        <location filename="MainWindow.ui" line="79"/>
+        <location filename="MainWindow.cpp" line="202"/>
+        <source>Apps name</source>
+        <translation>Názvy aplikácií</translation>
+    </message>
+    <message>
+        <location filename="MainWindow.ui" line="84"/>
+        <location filename="MainWindow.cpp" line="202"/>
+        <source>Untranslated</source>
+        <translation>Nepreložené</translation>
+    </message>
+    <message>
+        <location filename="MainWindow.ui" line="89"/>
+        <source>hiddenColumn</source>
+        <translation>skrytýStĺpec</translation>
+    </message>
+    <message>
+        <location filename="MainWindow.ui" line="102"/>
+        <location filename="MainWindow.cpp" line="721"/>
+        <source>Save current list</source>
+        <translation>Uložiť aktuálny zoznam</translation>
+    </message>
+    <message>
+        <location filename="MainWindow.ui" line="124"/>
+        <source>Information :</source>
+        <translation>Informácie:</translation>
+    </message>
+    <message>
+        <location filename="MainWindow.ui" line="132"/>
+        <source>Type :</source>
+        <translation>Typ:</translation>
+    </message>
+    <message>
+        <location filename="MainWindow.ui" line="187"/>
+        <source>File :</source>
+        <translation>Súbor:</translation>
+    </message>
+    <message>
+        <location filename="MainWindow.ui" line="218"/>
+        <source>Line number :</source>
+        <translation>Číslo riadku:</translation>
+    </message>
+    <message>
+        <location filename="MainWindow.ui" line="273"/>
+        <source>Tools</source>
+        <translation>Nástroje</translation>
+    </message>
+    <message>
+        <location filename="MainWindow.ui" line="286"/>
+        <source>Options</source>
+        <translation>Voľby</translation>
+    </message>
+    <message>
+        <location filename="MainWindow.cpp" line="86"/>
+        <source>No XML files found</source>
+        <translation>Nenájdené žiadne súbory XML</translation>
+    </message>
+    <message>
+        <location filename="MainWindow.cpp" line="86"/>
+        <source>No xml files are found. Make sure you launch TranslationTool in source folder or that your custom configuration is correct.</source>
+        <translation>Nenašli sa žiadne súbory xml. Uistite sa, či ste spustili nástroj TranslationTool v zdrojovom priečinku, alebo či je vaša vlastná konfigurácia správna.</translation>
+    </message>
+    <message>
+        <location filename="MainWindow.cpp" line="166"/>
+        <source>unused</source>
+        <translation>nepoužité</translation>
+    </message>
+    <message>
+        <location filename="MainWindow.cpp" line="191"/>
+        <source>No translation needed, good work!</source>
+        <translation>Nie je potrebné žiadne prekladanie. Dobrá práca!</translation>
+    </message>
+    <message>
+        <location filename="MainWindow.cpp" line="200"/>
+        <source>Progress:</source>
+        <translation>Priebeh:</translation>
+    </message>
+    <message>
+        <location filename="MainWindow.cpp" line="463"/>
+        <location filename="MainWindow.cpp" line="514"/>
+        <location filename="MainWindow.cpp" line="553"/>
+        <source>File not found</source>
+        <translation>Súbor sa nenašiel</translation>
+    </message>
+    <message>
+        <location filename="MainWindow.cpp" line="463"/>
+        <source>The file filter.xml was not found, make sure you launch TranslationTool in source folder or that your custom configuration is correct.</source>
+        <translation>Súbor filter.xml sa nenašiel. Uistite sa, či ste spustili nástroj TranslationTool v zdrojovom priečinku, alebo či je vaša vlastná konfigurácia správna.</translation>
+    </message>
+    <message>
+        <location filename="MainWindow.cpp" line="514"/>
+        <source>The file language.xml was not found, make sure you launch TranslationTool in source folder or that your custom configuration is correct.</source>
+        <translation>Súbor language.xml sa nenašiel. Uistite sa, či ste spustili nástroj TranslationTool v zdrojovom priečinku, alebo či je vaša vlastná konfigurácia správna.</translation>
+    </message>
+    <message>
+        <location filename="MainWindow.cpp" line="553"/>
+        <source>The file project.xml was not found, make sure you launch TranslationTool in source folder or that your custom configuration is correct.</source>
+        <translation>Súbor project.xml sa nenašiel. Uistite sa, či ste spustili nástroj TranslationTool v zdrojovom priečinku, alebo či je vaša vlastná konfigurácia správna.</translation>
+    </message>
+</context>
+<context>
+    <name>OptionWindow</name>
+    <message>
+        <location filename="OptionWindow.ui" line="23"/>
+        <source>Options</source>
+        <translation>Voľby</translation>
+    </message>
+    <message>
+        <location filename="OptionWindow.ui" line="31"/>
+        <source>Editor command :</source>
+        <translation>Príkaz editora:</translation>
+    </message>
+    <message>
+        <location filename="OptionWindow.ui" line="47"/>
+        <source>Use %1 for the path file and %2 for the line (for example : &quot;gedit +%2 %1&quot;)</source>
+        <translation>Použite %1 pre cestu k súboru a %2 pre riadok (napríklad: &quot;gedit +%2 %1&quot;)</translation>
+    </message>
+</context>
+</TS>
